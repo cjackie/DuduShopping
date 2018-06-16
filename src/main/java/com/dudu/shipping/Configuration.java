c85815c269e6f81f8f3012cd5fb567f4462239d7
cjackie <chaojie566@gmail.com>
2018-06-15 23:10:37 -0400

    UPS proxy

--- src/main/java/com/dudu/shipping/Configuration.java
+++ src/main/java/com/dudu/shipping/Configuration.java
@@ -11,7 +11,15 @@ public class Configuration {
      *
      * @param properties
      */
-    public static void configure(Properties properties) {
+    public static void configure(Properties properties) throws Exception {
+        String upsUrl = properties.getProperty("UPS_URL");
+        String upsAccessKey = properties.getProperty("UPS_ACCESS_KEY");
+        String upsUsername = properties.getProperty("UPS_USERNAME");
+        String userPassword = properties.getProperty("UPS_PASSWORD");
 
+        if (upsUrl == null || upsAccessKey == null || upsUsername == null || userPassword == null)
+            throw new IllegalStateException("Missing configurations for UPS");
+
+        UPSProxy.configure(upsUrl, upsAccessKey, upsUsername, userPassword);
     }
 }
--- src/main/java/com/dudu/shipping/UPSProxy.java
+++ src/main/java/com/dudu/shipping/UPSProxy.java
@@ -1,12 +1,18 @@
 package com.dudu.shipping;
 
+import com.dudu.net.HttpRequest;
+import com.dudu.net.HttpResponse;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
 import javax.net.ssl.HttpsURLConnection;
 import java.net.URL;
-import java.util.Date;
-import java.util.LinkedHashMap;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class UPSProxy implements ShippingTracker {
     private final static UPSProxy instance = new UPSProxy();
@@ -20,7 +26,7 @@ public class UPSProxy implements ShippingTracker {
         return instance;
     }
 
-    public static void configure(String api, String apiKey, String username, String password) {
+    static void configure(String api, String apiKey, String username, String password) {
         UPSProxy.api = api;
         UPSProxy.apiKey = apiKey;
         UPSProxy.username = username;
@@ -34,16 +40,119 @@ public class UPSProxy implements ShippingTracker {
         if (!identify(id))
             return null;
 
+        String requestData = "       " +
+                "        {\n" +
+                "            \"UPSSecurity\": {\n" +
+                "                \"UsernameToken\": {\n" +
+                "                    \"Username\": \"%s\",\n" +
+                "                    \"Password\": \"%s\"\n" +
+                "                },\n" +
+                "                \"ServiceAccessToken\": {\n" +
+                "                    \"AccessLicenseNumber\": \"%s\"\n" +
+                "                }\n" +
+                "            },\n" +
+                "            \"TrackRequest\": {\n" +
+                "                \"Request\": {\n" +
+                "                    \"RequestOption\": \"activity\",\n" +
+                "                    \"TransactionReference\": {\n" +
+                "                        \"CustomerContext\": \"Your Test Case Summary Description\"\n" +
+                "                    }\n" +
+                "                },\n" +
+                "                \"InquiryNumber\": \"%s\"\n" +
+                "            }\n" +
+                "        }";
+        requestData = String.format(requestData, username, password, apiKey, id);
+
+        // request object
+        HttpRequest httpRequest = new HttpRequest(api);
+        httpRequest.method(HttpRequest.GET)
+                .body(requestData);
+
+        JSONObject trackingResponse = null;
         try {
-            URL url = new URL(api);
+            HttpResponse response =  httpRequest.doRequest();
+            if (response.status() != 200)
+                return null;
 
-            HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
-
+            trackingResponse = new JSONObject(new JSONTokener(response.responseText())).getJSONObject("TrackResponse");
 
 
         } catch (Exception e) {
-            logger.warn("Unable to track " + id, e);
+            logger.warn("Failed to get shipping event.", e);
+            return null;
         }
+
+        // try Activity
+        try {
+            JSONArray activities = trackingResponse.getJSONObject("Shipment").getJSONArray("Activity");
+            SortedMap<Date, ShippingEvent> activiesMap = new TreeMap<>(Date::compareTo);
+
+            for (int i = 0; i < activities.length(); i++) {
+                JSONObject activity = activities.getJSONObject(i);
+                String city = activity.getJSONObject("ActivityLocation").getString("City");
+                String stateProvinceCode = activity.getJSONObject("ActivityLocation").getString("StateProvinceCode");
+                String description = activity.getString("Description");
+                String date = activity.getString("Date");
+                String time = activity.getString("Time");
+
+                String datetime = date + " " + time;
+                Date trackTime = null;
+                SimpleDateFormat dateFormat1 = new SimpleDateFormat("yyyyMMdd hhmmss");
+                SimpleDateFormat dateFormat2 = new SimpleDateFormat("yyyyMMdd hhmm");
+
+                try {
+                    trackTime = dateFormat1.parse(datetime);
+                } catch (ParseException ignore) { }
+
+                if (trackTime == null) {
+                    trackTime = dateFormat2.parse(datetime);
+                }
+
+                ShippingEvent event = new ShippingEvent();
+                event.setEventTime(trackTime);
+                event.setDescription(String.format("At %s %s, %s", city, stateProvinceCode, description));
+                activiesMap.put(trackTime, event);
+
+            }
+
+            return new LinkedHashMap<>(activiesMap);
+        } catch (Exception ignored) { }
+
+        /** case 2 **/
+
+        try {
+            JSONObject activity = trackingResponse.getJSONObject("Shipment").getJSONObject("Package").getJSONObject("Activity");
+            String city = activity.getJSONObject("ActivityLocation").getJSONObject("Address").getString("City");
+            String stateProvinceCode = activity.getJSONObject("ActivityLocation").getJSONObject("Address").getString("StateProvinceCode");
+
+            String date = activity.getString("Date");
+            String time = activity.getString("Time");
+            String description = activity.getJSONObject("Status").getString("Description");
+
+            String datetime = date + " " + time;
+            Date trackTime = null;
+            SimpleDateFormat dateFormat1 = new SimpleDateFormat("yyyyMMdd hhmmss");
+            SimpleDateFormat dateFormat2 = new SimpleDateFormat("yyyyMMdd hhmm");
+
+            try {
+                trackTime = dateFormat1.parse(datetime);
+            } catch (ParseException ignore) { }
+
+            if (trackTime == null) {
+                trackTime = dateFormat2.parse(datetime);
+            }
+
+            ShippingEvent event = new ShippingEvent();
+            event.setEventTime(trackTime);
+            event.setDescription(String.format("At %s %s, %s", city, stateProvinceCode, description));
+
+            LinkedHashMap<Date, ShippingEvent> activities = new LinkedHashMap<>();
+            activities.put(trackTime, event);
+
+            return activities;
+        } catch (Exception igored) { }
+
+        logger.warn("Failed to track UPS: " + id);
         return null;
     }
 
